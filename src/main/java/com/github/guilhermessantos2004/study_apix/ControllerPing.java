7f3e4e7e5c0a0a8d695665b44e3755996336c40d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-17 22:45:06 -0300

    fisrt commit

---
+++ src/main/java/com/github/guilhermessantos2004/study_apix/ControllerPing.java
@@ -0,0 +1,14 @@
+package com.github.guilhermessantos2004.study_apix;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("ping")
+public class ControllerPing { 
+    @GetMapping
+    public String ping(){
+        return "pong";
+    }   
+}
