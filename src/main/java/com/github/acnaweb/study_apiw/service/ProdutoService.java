6e0f8310f9e80b73a192cee99b08c1fc94c8e4fb
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-31 22:56:45 -0300

    bug: Produto

--- src/main/java/com/github/acnaweb/study_apiw/service/ProdutoService.java
+++ src/main/java/com/github/acnaweb/study_apiw/service/ProdutoService.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.github.acnaweb.study_apiw.dto.ProdutoRequestCreate;
 import com.github.acnaweb.study_apiw.model.Produto;
 import com.github.acnaweb.study_apiw.repository.ProdutoRepository;
 
@@ -14,7 +16,11 @@ public class ProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    public Produto save(Produto produto) {        
+    public Produto save(ProdutoRequestCreate dto) {       
+        
+        Produto produto = new Produto();
+        produto.setNome(dto.getNome());
+        
         return produtoRepository.save(produto);
     }
 
